fa348d2696cb05624e2665b1c1d9b9530940820a
EuronRomanov <juanpablo.gaona@yahoo.com>
2023-06-12 21:37:13 -0500

    login add

---
+++ Farmacia/src/com/farmacia/bd/ConexionBD.java
@@ -0,0 +1,48 @@
+package com.farmacia.bd;
+
+import java.sql.*;
+
+
+public class ConexionBD {
+
+    public static String url = "jdbc:mysql://localhost/farmacias?serverTimezone=UTC";
+    public static String usuario = "root";
+    public static String clave = "sacapalca";
+    public static String clase = "com.mysql.cj.jdbc.Driver";
+    private static Connection conexion = null;
+    public static Connection conectar() {
+       
+
+        try {
+            Class.forName(clase);
+            conexion = DriverManager.getConnection(url, usuario, clave);
+            //System.out.println("Conexion Establecida");
+            //JOptionPane.showMessageDialog(null, "CONEXION EXITOSA");
+        } catch (ClassNotFoundException e) {
+			System.out.println("Error >> Driver no Instalado" + e.getMessage());
+		} catch (SQLException e) {
+			System.out.println("Error >> de conexión con la BD" + e.getMessage());
+		} catch (Exception e) {
+			System.out.println("Error >> general : " + e.getMessage());
+		} 
+        return conexion;
+    }
+    public static Connection CerrarConexion()
+    {
+        try {
+			if (conexion.isClosed()) {
+				try {
+					conexion.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+            
+        return conexion;
+    }
+}
---
+++ Farmacia/src/com/farmacia/form/Main.java
@@ -0,0 +1,60 @@
+package com.farmacia.form;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.awt.CardLayout;
+import javax.swing.JTabbedPane;
+import javax.swing.JLayeredPane;
+import javax.swing.JDesktopPane;
+
+public class Main extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main frame = new Main();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Main() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnNewMenu = new JMenu("New menu");
+		menuBar.add(mnNewMenu);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("New menu item");
+		mnNewMenu.add(mntmNewMenuItem);
+		
+		JMenuItem mntmNewMenuItem_1 = new JMenuItem("New menu item");
+		mnNewMenu.add(mntmNewMenuItem_1);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(new CardLayout(0, 0));
+	}
+}
