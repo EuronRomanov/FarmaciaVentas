98f98c3e3afba50ecab34394e7f349ceccf6d391
HP <HP@COMPAQ.local>
2023-06-23 02:50:34 -0500

    funciona documento descarga d documento

--- Farmacia/src/com/farmacia/utils/GenerdorDocumentos.java
+++ Farmacia/src/com/farmacia/utils/GenerdorDocumentos.java
@@ -1,8 +1,14 @@
 package com.farmacia.utils;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.regex.Matcher;
 
 import javax.swing.JFileChooser;
 
@@ -10,6 +16,7 @@ import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Image;
+import com.itextpdf.text.PageSize;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.Rectangle;
 import com.itextpdf.text.pdf.BarcodeEAN;
@@ -20,13 +27,17 @@ public class GenerdorDocumentos {
 
 	
 	
-	public void generarPDFs(String codigo, int cantidad)  {
+	public void generarPDFs(String codigo, int cantidad,String carpetaSeleccionada)  {
 		
 		try {
-			Document doc=new Document();
-			JFileChooser selectCarpeta=new JFileChooser();
-			selectCarpeta.setCurrentDirectory(new File("."));
-			PdfWriter pdf=PdfWriter.getInstance(doc, new FileOutputStream("/Users/janpierarmijos/Desktop/codigo.pdf"));
+			//BufferedWriter writer=Files.newBufferedWriter(Paths.get(carpetaSeleccionada.getAbsolutePath()+File.separator+"codigo.pdf"));
+
+			//String h=Paths.get(carpetaSeleccionada.getAbsolutePath()+File.separator+"codigo.pdf").toString();
+			String h=carpetaSeleccionada.replaceAll( Matcher.quoteReplacement(File.separator), "/")+"/codigo1.pdf";
+			//System.out.println("Desde metodo "+h);
+			Document doc=new Document(PageSize.A4);
+			
+			PdfWriter pdf=PdfWriter.getInstance(doc, new FileOutputStream(h));
 			pdf.open();
 			doc.open();
 			BarcodeEAN code=new BarcodeEAN();
@@ -53,6 +64,9 @@ public class GenerdorDocumentos {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
