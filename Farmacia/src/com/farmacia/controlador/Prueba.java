fed430d3c276c0528b50226a4e74aa3b765820c8
EuronRomanov <juanpablo.gaona@yahoo.com>
2023-06-22 21:26:23 -0500

    genero pdf de coodigo barras

--- Farmacia/src/com/farmacia/controlador/Prueba.java
+++ Farmacia/src/com/farmacia/controlador/Prueba.java
@@ -3,13 +3,15 @@ package com.farmacia.controlador;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+import com.farmacia.utils.GenerdorDocumentos;
+
 public class Prueba {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String fecha="2023-06-20 16:25:23.0";
-		   DateTimeFormatter parser = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.S");
-		LocalDateTime dateTime=LocalDateTime.parse(fecha,parser);
+		 
+		GenerdorDocumentos generador=new GenerdorDocumentos();
+		generador.generarPDFs("1999999999999", 13);
 	}
 
 }
