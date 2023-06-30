8aebfeb23b0691a6f38e6fa2feb3268e65b78352
HP <HP@COMPAQ.local>
2023-06-30 05:24:11 -0500

    probe si generaba e impirmir documento

--- Farmacia/src/com/farmacia/controlador/Prueba.java
+++ Farmacia/src/com/farmacia/controlador/Prueba.java
@@ -5,16 +5,32 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 import com.farmacia.utils.GenerdorDocumentos;
+import java.awt.print.PrinterException;
+import java.awt.print.PrinterJob;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.printing.PDFPageable;
 
 public class Prueba {
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 
 		GenerdorDocumentos generador=new GenerdorDocumentos();
 		
-		generador.generarTicketTest(1);
+		
+		
+			generador.generarTicket(1);
+			//new Prueba().imprimir();
 		
 	}
+	
+	
 
 }
