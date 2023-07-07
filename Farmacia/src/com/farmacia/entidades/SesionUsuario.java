92158c40382190790575da324cc0a04323db8d6a
HP <HP@COMPAQ.local>
2023-07-07 02:17:52 -0500

    almacena login

---
+++ Farmacia/src/com/farmacia/entidades/SesionUsuario.java
@@ -0,0 +1,20 @@
+package com.farmacia.entidades;
+
+public class SesionUsuario {
+private static int codUsuario;
+private static  String nombreUsuario;
+public static int getCodUsuario() {
+	return codUsuario;
+}
+public static String getNombreUsuario() {
+	return nombreUsuario;
+}
+public static void setNombreUsuario(String nombreUsuario) {
+	SesionUsuario.nombreUsuario = nombreUsuario;
+}
+public static void setCodUsuario(int codUsuario) {
+	SesionUsuario.codUsuario = codUsuario;
+}
+
+
+}
