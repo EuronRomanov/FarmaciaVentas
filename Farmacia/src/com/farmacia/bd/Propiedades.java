ba7fe0b012d88b5878c458ce04e1764a4acf57e7
HP <HP@COMPAQ.local>
2023-07-21 22:17:17 -0500

    se agrego direccion de la direccion del archvo jar

--- Farmacia/src/com/farmacia/bd/Propiedades.java
+++ Farmacia/src/com/farmacia/bd/Propiedades.java
@@ -6,14 +6,22 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URISyntaxException;
 import java.util.Properties;
+import java.io.File;
+
 
 public class Propiedades {
+	
+	
+	private  String ruta=System.getProperty("java.class.path").replaceFirst("farmacia.exe", ".env");
   public Properties cargarArchivoProperties() {
+	  createArchivo();
 	  Properties propiedades=new Properties();
 	  InputStream input;
 	try {
-		input = new FileInputStream("src/com/farmacia/utils/app.properties");
+		input = new FileInputStream( ruta);
+		//input=getClass().getResourceAsStream("/app.properties");
 		propiedades.load( input);
 	} catch (FileNotFoundException e) {
 		// TODO Auto-generated catch block
@@ -26,10 +34,15 @@ public class Propiedades {
 	  
   }
   public void escribirArchivoProperties(String baseDatos,String host,String usuario,String password,String port) {
+	  createArchivo();
 	  Properties propiedades=new Properties();
 	  OutputStream output;
 	try {
-		 output = new FileOutputStream("src/com/farmacia/utils/app.properties");
+		
+		
+		// output = new FileOutputStream("./resources/app.properties");
+		output = new FileOutputStream(ruta);
+		
 		 propiedades.setProperty("db.localhost", host);
 			
 		 propiedades.setProperty("db.database", baseDatos);
@@ -47,4 +60,30 @@ public class Propiedades {
 	  
 	  
   }
+  
+  private void createArchivo() {
+	  try {
+             /*String ruta=getClass()
+                     .getProtectionDomain()
+                     .getCodeSource()
+                     .getLocation()
+                     .toURI()
+                     .getPath();*/
+		 
+	        File New_File = new File(ruta);
+
+	        if (New_File.createNewFile()){
+	            System.out.println("The file is created successfully!");
+	        }
+	        else{
+	            System.out.println("The file already exists.");
+	        }
+
+	        }
+	        catch (IOException e) {
+	            e.printStackTrace();
+	        }
+
+  }
+  
 }
